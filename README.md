60d6bb57a6965fcb65e82f3536b9a4722ee7db9b
Theresa Head <nerdgirl.ent@gmail.com>
2020-03-17 23:00:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Base CRA with bootstrap (sass config), eslint, and prettier
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './assets/sass/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>React Template</h1>
+            <p>Using Bootstrap</p>
+            <button className="btn btn-primary" type="button">
+              Example
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
