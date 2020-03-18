6d1077cd24cb3ed12c8e0704a7387ac452a5081a
Theresa Head <nerdgirl.ent@gmail.com>
2020-03-18 01:05:09 -0700

    initial request setup

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-Base CRA with bootstrap (sass config), eslint, and prettier
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './assets/sass/index.scss';
+import Header from './components/Header';
+import Pagination from './components/Pagination';
+import Posts from './components/Posts';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [after, setAfter] = useState(null);
+  // const [permalink, setPermalink] = useState(null);
+
+  // const commentsEndpoint = "https://www.reddit.com" + commentId + ".json";
+
+  useEffect(() => {
+    const category = 'top';
+    console.log('1 after', after);
+    // console.log('1 permalink', permalink);
+    axios({
+      method: 'get',
+      baseURL: 'https://www.reddit.com',
+      url: `/r/all/${category}.json`,
+      params: {
+        limit: 5,
+        after,
+      },
+    }).then((res) => {
+      const after = res.data.data.after;
+      const post = res.data.data.children;
+      // const permalink = res.data.data.children.permalink;
+      console.log('2 after', after);
+      // console.log('2 permalink', permalink);
+      console.log('posts', post);
+      setAfter(after);
+      // setPermalink(permalink);
+      setPosts(post);
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1>React Template</h1>
-            <p>Using Bootstrap</p>
-            <button className="btn btn-primary" type="button">
-              Example
-            </button>
+            <Header title="Reddit Client" />
+            <Posts list={posts} />
+            <Pagination />
           </div>
         </div>
       </div>
